3c0a7d85fb586b793af623e60b676cca1d264990
danielchicana <74795033+danielchicana@users.noreply.github.com>
2021-05-11 22:02:47 -0500

    Daniel chicana cambios iterators.h

--- iterators.h
+++ iterators.h
@@ -4,9 +4,8 @@
 
 #include <iostream>
 #include <vector>
-#include <Container>
 #include "type.h"
-
+using namespace std;
 template <typename Iterator>
 void recorrer1(Iterator begin, Iterator end, ostream &os)
 {
@@ -18,27 +17,34 @@ void recorrer1(Iterator begin, Iterator end, ostream &os)
 }
 
 template <typename Iterator, typename OF>
-void recorrer2(Iterator begin, Iterator end, OF &of)
+void recorrer2(Iterator begin, Iterator end, OF &&of)
 {
-	while (begin != end)
+	while (begin != end){
 		of(*begin);
+    begin++;
+  }
 }
 
 template <typename Iterator, typename OF, typename Extra>
-void recorrer3(Iterator begin, Iterator end, OF &of, Extra &extra_param)
+void recorrer3(Iterator begin, Iterator end, OF &&of, Extra &extra_param)
 {
-	while (begin != end)
+	while (begin != end){
 		of(*begin, extra_param);
+    begin++;
+  }
 }
 
 template <typename Container, typename Function>
 void recorrer4(Container &container, Function &function)
 {
   auto begin = container.begin();
-	while (begin != container.end())
+	while (begin != container.end()){
 		function(*begin); //eliminado extra_param_MaizoDiego
+    begin++;
+  }
 }
 
+template <typename T>
 void incrementar(T &val) //int cambiado por T1 - MaizoDiego
 {
 	val++;
@@ -48,7 +54,7 @@ template <typename Container>
 void print(Container &container, ostream &os)
 {
 	auto begin = container.begin();
-	while (begin != container.end())
+	for( ; begin != container.end() ; begin++ )
 		os << *begin << endl;
 }
 #endif // Agregado por Kevin De Lama
